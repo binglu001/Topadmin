5f148266bf0c8b7807e562581f090154407e5bd1
woooowen <woooowen@gmail.com>
2014-10-04 14:25:12 +0800

    v0.1
    1.登陆页面布局
    2.呼出键盘
    3.点击其他区域隐藏键盘

--- ViewController.swift
+++ ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uNameLabel: UITextField!
+    
+    @IBOutlet weak var pwdLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        uNameLabel.resignFirstResponder()
+        pwdLabel.resignFirstResponder()
+        
+    }
+    @IBAction func LoginBtn(sender: AnyObject) {
+        
+    }
 
 }
 
