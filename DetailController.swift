ee2543f95d624a236117cfd434d347c8f8c4b9ee
woooowen <woooowen@gmail.com>
2014-10-06 22:43:10 +0800

    add detail

---
+++ DetailController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailController.swift
+//  Topadmin
+//
+//  Created by james on 14-10-6.
+//  Copyright (c) 2014年 woowen. All rights reserved.
+//
+import UIKit
+
+class DetailController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    
+}
--- ListvController.swift
+++ ListvController.swift
@@ -27,7 +27,7 @@ class ListvController: UIViewController ,UITableViewDataSource,UITableViewDelega
         let timeLineUrl = "http://top.mogujie.com/app_top_v142_timeline/pubtimeline?_uid=1jf4k&_swidth=720&timestamp=1412437672&_channel=NAOtop&_atype=android&_mgj=541648ca87e6ca9e8d9a1790639c34351412437672&_sdklevel=18&_network=2&sign=pZqBZDt0XE1Ss7UX0u6UxfYr1RtfyOuyU04ahLrLpHGuJEDWSRfBiWCISM7YBEGWO%2BhjijAT7CVtlxe%2FAPl%2BNA%3D%3D&mbook=&_aver=142&_fs=NAOtop142&_did=99000537220553&_source=NAOtop142"
         eHttp.delegate = self
         eHttp.onSearchUrl(timeLineUrl)
-
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -96,6 +96,7 @@ class ListvController: UIViewController ,UITableViewDataSource,UITableViewDelega
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
         //self.dismissViewControllerAnimated(true, completion: nil)
+        self.performSegueWithIdentifier("detail", sender: self)
     }
     
     func didRecieveResult(result: NSDictionary){
