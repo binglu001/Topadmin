106b53409b977345448fe6591d04bc72b3a6a82b
woooowen <woooowen@gmail.com>
2014-10-14 23:11:32 +0800

    fix

--- ListvController.swift
+++ ListvController.swift
@@ -31,14 +31,11 @@ class ListvController: UIViewController ,UITableViewDataSource,UITableViewDelega
         
         let timeLineUrl = "http://top.mogujie.com/app_top_v142_timeline/pubtimeline?_uid=1jf4k&_swidth=720&timestamp=1412437672&_channel=NAOtop&_atype=android&_mgj=541648ca87e6ca9e8d9a1790639c34351412437672&_sdklevel=18&_network=2&sign=pZqBZDt0XE1Ss7UX0u6UxfYr1RtfyOuyU04ahLrLpHGuJEDWSRfBiWCISM7YBEGWO%2BhjijAT7CVtlxe%2FAPl%2BNA%3D%3D&mbook=&_aver=142&_fs=NAOtop142&_did=99000537220553&_source=NAOtop142"
         
-        
         eHttp.delegate = self
         eHttp.onSearchUrl(timeLineUrl)
-
         //添加下拉刷新手势
-
         refreshControl.addTarget(self, action: "refreshData", forControlEvents: UIControlEvents.ValueChanged)
-        refreshControl.attributedTitle = NSAttributedString(string: "松手刷新新闻")
+        refreshControl.attributedTitle = NSAttributedString(string: "松手刷新")
         tableView.addSubview(refreshControl)
     }
     
